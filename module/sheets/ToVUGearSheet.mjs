78d1d1abfbb16a2f894228663c2afd9b1f0eb81d
Andrew Fillhart <afillhar@bard.edu>
2024-03-20 11:40:14 -0400

    Storage bug worked out. Looking into 2ndary change bug on some pages.

--- module/sheets/ToVUGearSheet.mjs
+++ module/sheets/ToVUGearSheet.mjs
@@ -25,69 +25,65 @@ export default class GearSkillsSheet extends FormApplication {
         let gearSetup = this.actor.system.gear;
 
         // Simple Melee
-        if(!gearSetup.weapons.simple){
-            if(!gearSetup.weapons.simple.melee){
-                console.log(">>> Inside simple melee check");
-                for (const [key, label] of Object.entries(tovu.weaponList.simple.melee)){
-                    console.log(">> in for loop");
-                    const path = `weapons.simple.melee.${key}`;
-                    setProperty(gearSetup, path, {label: game.i18n.localize(label)});
-                }
-            }
-            // Simple Range
-            if(!gearSetup.weapons.simple.range){
-                for (const [key, label] of Object.entries(tovu.weaponList.simple.range)){
-                    const path = `weapons.simple.range.${key}`;
-                    setProperty(gearSetup, path, {label: game.i18n.localize(label)});
-                }
+        if(!gearSetup.weapons?.simple?.melee){
+            for (const [key, label] of Object.entries(tovu.weaponList.simple.melee)){
+                const path = `weapons.simple.melee.${key}`;
+                setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
         }
-        if(Object.keys(gearSetup.weapons.martial).length === 0){
-            // Martial Melee
-            if(Object.keys(gearSetup.weapons.martial.melee).length === 0){
-                for (const [key, label] of Object.entries(tovu.weaponList.martial.melee)){
-                    const path = `weapons.martial.melee.${key}`;
-                    setProperty(gearSetup, path, {label: game.i18n.localize(label)});
-                }
+        // Simple Range
+        if(!gearSetup.weapons?.simple?.range){
+            for (const [key, label] of Object.entries(tovu.weaponList.simple.range)){
+                const path = `weapons.simple.range.${key}`;
+                setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
-            // Martial Range
-            if(Object.keys(gearSetup.weapons.martial.range).length === 0){
-                for (const [key, label] of Object.entries(tovu.weaponList.martial.range)){
-                    const path = `weapons.martial.range.${key}`;
-                    setProperty(gearSetup, path, {label: game.i18n.localize(label)});
-                }
+        }
+        // Martial Melee
+        if(!gearSetup.weapons?.martial?.melee){
+            for (const [key, label] of Object.entries(tovu.weaponList.martial.melee)){
+                const path = `weapons.martial.melee.${key}`;
+                setProperty(gearSetup, path, {label: game.i18n.localize(label)});
+            }
+        }
+        // Martial Range
+        if(!gearSetup.weapons?.martial?.range){
+            for (const [key, label] of Object.entries(tovu.weaponList.martial.range)){
+                const path = `weapons.martial.range.${key}`;
+                setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
         }
         // Tools: Artisan
-        if(Object.keys(gearSetup.tools.artisan).length === 0){
+        if(!Object.keys(gearSetup.tools.artisan).length){
+            console.log(">>> In if statement");
             for (const [key, label] of Object.entries(tovu.tools.artisan)){
+                console.log(">> in for loop, key:", key, "label:", game.i18n.localize(label));
                 const path = `tools.artisan.${key}`;
                 setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
         }
         // Tools: Unique
-        if(Object.keys(gearSetup.tools).length === 0){
+        if(!Object.keys(gearSetup.tools.unique).length){
             for (const [key, label] of Object.entries(tovu.tools.unique)){
                 const path = `tools.unique.${key}`;
                 setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
         }
         // Tools: Gaming
-        if(Object.keys(gearSetup.tools.gaming).length === 0){
+        if(!Object.keys(gearSetup.tools.gaming).length){
             for (const [key, label] of Object.entries(tovu.tools.gaming)){
                 const path = `tools.gaming.${key}`;
                 setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
         }
         // Tools: Instruments
-        if(Object.keys(gearSetup.tools.instruments).length === 0){
+        if(!Object.keys(gearSetup.tools.instruments).length){
             for (const [key, label] of Object.entries(tovu.tools.instruments)){
                 const path = `tools.instruments.${key}`;
                 setProperty(gearSetup, path, {label: game.i18n.localize(label)});
             }
         }
         // Tools: Vehicle
-        if(Object.keys(gearSetup.tools.vehicles).length === 0){
+        if(!Object.keys(gearSetup.tools.vehicles).length){
             for (const [key, label] of Object.entries(tovu.tools.vehicles)){
                 const path = `tools.vehicles.${key}`;
                 setProperty(gearSetup, path, {label: game.i18n.localize(label)});
@@ -114,15 +110,15 @@ export default class GearSkillsSheet extends FormApplication {
         const toolName = event.currentTarget.dataset.key;
         const isChecked = event.currentTarget.checked;
         const label = event.currentTarget.dataset.label;
+        console.log("Trait:",this.trait);
+        console.log("Category:",toolCategory);
+        console.log("Name:",toolName);
+        console.log("Label:",label);
 
         let updateData = {};
         let path = '';
-        if(this.trait === 'tools')
-            path = toolCategory === 'unique' ? `system.gear.${this.trait}.${toolName}` : `system.gear.${this.trait}.${toolCategory}.${toolName}`;
-        else{
-            path = `system.gear.${this.trait}.${toolCategory}.${toolName}`;
-            updateData[`${path}.label`] = label;
-        }
+        path = `system.gear.${this.trait}.${toolCategory}.${toolName}`;
+        updateData[`${path}.label`] = label;
         updateData[`${path}.value`] = isChecked;
         await this.actor.update(updateData);
         console.log(this.actor.system.gear);
